363a0abf7ff73ed3397f6d89798aa117fa4d17ba
jonga <jvfps20@gmail.com>
2024-09-26 02:22:53 -0300

    Create idade.cs

---
+++ idade.cs
@@ -0,0 +1,13 @@
+int idade = 80 ;
+switch (idade)
+{
+    case 16:
+        Console.WriteLine(" adolescente ");
+        break;
+    case 30:
+        Console.WriteLine("adulto");
+        break;
+    case 80:
+        Console.WriteLine("idoso");
+        break;
+}
